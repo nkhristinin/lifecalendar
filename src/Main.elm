2b70742ed6d816678bcf1ca00736f0acc1e67d5e
Nikita Khristinin <nkhristinin@gmail.com>
2018-01-14 19:37:47 +0200

    Add link to github

--- src/Main.elm
+++ src/Main.elm
@@ -200,17 +200,22 @@ viewFooter =
         [ p []
             [ text "Using "
             , a [ href "http://elm-lang.org/", rel "noopener", target "_blank" ]
-                [ text "Elm." ]
+                [ text "Elm" ]
+            ]
+        , p []
+            [ a [ href "https://github.com/hrnik/lifecalendar", rel "noopener", target "_blank" ]
+                [ text "Source code" ]
+            , text " on GitHub"
             ]
         , p []
             [ text "Inspired by this "
             , a [ href "(https://medium.com/design-productivity/%D0%BA%D0%B0%D0%BB%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D1%8C-%D0%B6%D0%B8%D0%B7%D0%BD%D0%B8-fac1327d676c)", rel "noopener", target "_blank" ]
-                [ text "article." ]
+                [ text "article" ]
             ]
         , p []
             [ text "Icons from "
             , a [ href "https://icons8.com", rel "noopener", target "_blank" ]
-                [ text "Icons8.com." ]
+                [ text "Icons8.com" ]
             ]
         ]
 
